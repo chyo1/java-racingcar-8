f6b42106e35ae963a88f48cef6dd8fbb3f649f5e
chyo1 <hyowon3856@gmail.com>
2025-10-27 14:33:53 +0900

    feat : Car constructor with name

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -2,9 +2,13 @@ package racingcar;
 
 public class Car {
     private String name;
-    private int location;
+    private int movineCount;
 
     // 생성자
+    public Car(String name) {
+        this.name = name;
+        movineCount = 1;
+    }
 
     // 전진 횟수 출력
 }
