201d3f09baa762586d78f946f91470537f6f86d4
chyo1 <hyowon3856@gmail.com>
2025-10-27 16:26:53 +0900

    refactor : 차 이름 입력 에러 판단 로직 간결하게 변경

--- src/main/java/racingcar/Error.java
+++ src/main/java/racingcar/Error.java
@@ -4,18 +4,8 @@ public class Error {
     public void checkCarNamesInput(String carNamesInput) {
         String[] inputs = carNamesInput.split(",");
         for (String input : inputs) {
-
-            // 이름이 5자 초과인 경우
-            if (input.length() > 5) {
+            if (input.length() > 5 || input.chars().anyMatch(c -> !Character.isLetter(c))) {
                 throw new IllegalArgumentException("5자 이내의 알파벳만 입력 가능합니다.");
-            }
-
-            for (Character c : input.toCharArray()) {
-                // 알파벳이 아닌 문자가 차의 이름으로 입력될 경우
-                // 구분자로 ,가 아닌 특수문자가 들어온 경우
-                if (!Character.isLetter(c)) {
-                    throw new IllegalArgumentException("5자 이내의 알파벳만 입력 가능합니다.");
-                }
             }
         }
     }
