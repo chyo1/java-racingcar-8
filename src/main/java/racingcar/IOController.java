12f067f1b75b43c999070638d102491bbc69848d
chyo1 <hyowon3856@gmail.com>
2025-10-27 14:10:53 +0900

    feat : 차 이름 입력 메서드

--- src/main/java/racingcar/IOController.java
+++ src/main/java/racingcar/IOController.java
@@ -1,7 +1,14 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class IOController {
     // 이름 입력
+    public String getCarNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉽표(,) 기준으로 구분)");
+        return Console.readLine();
+    }
+    
     // 시도 횟수 입력
     // 우승자 출력
 }
