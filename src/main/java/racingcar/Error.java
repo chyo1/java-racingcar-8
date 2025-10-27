df7ced508b69d2ca618da5376a096cf68b15fccc
chyo1 <hyowon3856@gmail.com>
2025-10-27 15:30:00 +0900

    feat : check car names input error

--- src/main/java/racingcar/Error.java
+++ src/main/java/racingcar/Error.java
@@ -1,10 +1,24 @@
 package racingcar;
 
 public class Error {
-    // 이름이 5자 초과인 경우
-    // 알파벳이 아닌 문자가 차의 이름으로 입력될 경우
-    // 구분자로 ,가 아닌 특수문자가 들어온 경우
+    public void checkCarNamesInput(String carNamesInput) {
+        String[] inputs = carNamesInput.split(",");
+        for (String input : inputs) {
 
+            // 이름이 5자 초과인 경우
+            if (input.length() > 5) {
+                throw new IllegalArgumentException("5자 이내의 알파벳만 입력 가능합니다.");
+            }
+
+            for (Character c : input.toCharArray()) {
+                // 알파벳이 아닌 문자가 차의 이름으로 입력될 경우
+                // 구분자로 ,가 아닌 특수문자가 들어온 경우
+                if (!Character.isLetter(c)) {
+                    throw new IllegalArgumentException("5자 이내의 알파벳만 입력 가능합니다.");
+                }
+            }
+        }
+    }
 
     // 시도 횟수 입력 검증
     public void checkTrialCountInputError(String trialCountInput) {
