b4bac59a537481d10b8ea08ba6b2ea2d05da687e
chyo1 <hyowon3856@gmail.com>
2025-10-27 14:30:15 +0900

    feat : RacingProcess에서 ioController 실행

--- src/main/java/racingcar/RacingProcess.java
+++ src/main/java/racingcar/RacingProcess.java
@@ -1,17 +1,26 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
 
 public class RacingProcess {
+    IOController ioController = new IOController();
 
     public void run() {
         // 자동차 입력 받기
+        String carNames = ioController.getCarNames();
         // 각 자동차 객체 만들기
 
         // 실행 횟수 입력 받기
-
+        String trialCount = ioController.getTrialCount();
         // 실행 횟수만큼 반복
+
+        // 우승자 구하기
+
         // 우승자 출력
+        List<String> winners = null;
+        ioController.showWinner(winners);
+
         Console.close();
     }
 }
