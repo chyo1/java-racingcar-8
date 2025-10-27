b8b48b6bbeb7802ca66585b3a05108e90f08f2c0
chyo1 <hyowon3856@gmail.com>
2025-10-27 15:26:10 +0900

    feat : check trial count input error

--- src/main/java/racingcar/Error.java
+++ src/main/java/racingcar/Error.java
@@ -5,7 +5,16 @@ public class Error {
     // 알파벳이 아닌 문자가 차의 이름으로 입력될 경우
     // 구분자로 ,가 아닌 특수문자가 들어온 경우
 
-    // 시도 횟수가 자연수가 아닌 경우
-    // 시도 횟수가 음수인 경우
-    // 시도 횟수가 100회를 넘어갈 경우
+
+    // 시도 횟수 입력 검증
+    public void checkTrialCountInputError(String trialCountInput) {
+        try {
+            int trialCount = Integer.parseInt(trialCountInput);
+            if (trialCount <= 0 || 100 < trialCount) {
+                throw new IllegalArgumentException("1에서 100 사이의 자연수만 입력 가능합니다.");
+            }
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("숫자만 입력 가능합니다.");
+        }
+    }
 }
