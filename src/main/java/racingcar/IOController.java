f1361cacbef78370a59eeee45c4a320644c7432b
chyo1 <hyowon3856@gmail.com>
2025-10-27 14:11:46 +0900

    feat : 시도 횟수 입력

--- src/main/java/racingcar/IOController.java
+++ src/main/java/racingcar/IOController.java
@@ -8,7 +8,12 @@ public class IOController {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉽표(,) 기준으로 구분)");
         return Console.readLine();
     }
-    
+
     // 시도 횟수 입력
+    public String getTrialCount() {
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        return Console.readLine();
+    }
+
     // 우승자 출력
 }
