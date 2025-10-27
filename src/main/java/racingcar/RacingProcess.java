4cbe9dceb7afe43d6235798a004d6efcba954a1d
chyo1 <hyowon3856@gmail.com>
2025-10-27 15:50:09 +0900

    feat : RacingProcess로 기능 실행

--- src/main/java/racingcar/RacingProcess.java
+++ src/main/java/racingcar/RacingProcess.java
@@ -1,24 +1,42 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
 import java.util.List;
 
 public class RacingProcess {
-    IOController ioController = new IOController();
+    private IOController ioController = new IOController();
+    private Error error = new Error();
+    private Calculator calculator = new Calculator();
+
+    private List<Car> cars = new ArrayList<>();
 
     public void run() {
         // 자동차 입력 받기
         String carNames = ioController.getCarNames();
+
+        error.checkCarNamesInput(carNames);
+
         // 각 자동차 객체 만들기
+        String[] carNamesSplitBySeparator = carNames.split(",");
+        for (String carName : carNamesSplitBySeparator) {
+            cars.add(new Car(carName));
+        }
 
         // 실행 횟수 입력 받기
-        String trialCount = ioController.getTrialCount();
+        String trialCountInput = ioController.getTrialCount();
+        error.checkTrialCountInputError(trialCountInput);
+
         // 실행 횟수만큼 반복
+        int trialCount = Integer.parseInt(trialCountInput);
+        System.out.println("실행 결과");
+        for (int i = 0; i < trialCount; i++) {
+            calculator.movingCars(cars);
+            ioController.showResultsByStage(cars);
+        }
 
         // 우승자 구하기
-
-        // 우승자 출력
-        List<String> winners = null;
+        List<Car> winners = calculator.getWinners(cars);
         ioController.showWinner(winners);
 
         Console.close();
