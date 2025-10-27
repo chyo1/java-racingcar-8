e44560ed7ce233afff9cde35d03a7dcb2130cc7d
chyo1 <hyowon3856@gmail.com>
2025-10-27 16:31:22 +0900

    chore : 에러 출력 문구 수정

--- src/main/java/racingcar/Error.java
+++ src/main/java/racingcar/Error.java
@@ -5,7 +5,7 @@ public class Error {
         String[] inputs = carNamesInput.split(",");
         for (String input : inputs) {
             if (input.length() > 5 || input.chars().anyMatch(c -> !Character.isLetter(c))) {
-                throw new IllegalArgumentException("5자 이내의 알파벳만 입력 가능합니다.");
+                throw new IllegalArgumentException("차의 이름은 5자 이내의 알파벳만 입력 가능합니다.");
             }
         }
     }
@@ -16,11 +16,11 @@ public class Error {
         try {
             trialCount = Integer.parseInt(trialCountInput);
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("숫자만 입력 가능합니다.");
+            throw new IllegalArgumentException("시도 횟수는 숫자만 입력 가능합니다.");
         }
 
         if (trialCount <= 0 || 100 < trialCount) {
-            throw new IllegalArgumentException("1에서 100 사이의 자연수만 입력 가능합니다.");
+            throw new IllegalArgumentException("시도 횟수는 1에서 100 사이의 자연수만 입력 가능합니다.");
         }
     }
 }
