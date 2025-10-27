c4afacbe21176a3a80aff4b1d097b287ada17da4
chyo1 <hyowon3856@gmail.com>
2025-10-27 12:33:59 +0900

    feat : racingProcess 기능 draft

---
+++ src/main/java/racingcar/RacingProcess.java
@@ -0,0 +1,14 @@
+package racingcar;
+
+public class RacingProcess {
+
+    public void run() {
+        // 자동차 입력 받기
+        // 각 자동차 객체 만들기
+
+        // 실행 횟수 입력 받기
+
+        // 실행 횟수만큼 반복
+        // 우승자 출력
+    }
+}
